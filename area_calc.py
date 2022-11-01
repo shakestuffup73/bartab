1b4d5388da965d2909fe9ac5f12d22b4e918f6f0
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-31 21:23:48 -0400

    submit python bartab calc hw

---
+++ area_calc.py
@@ -0,0 +1,15 @@
+from bar_tab import Tab
+
+table1 = Tab()
+table1.add('soft_drink')
+table1.add('chicken')
+table1.add('dessert')
+
+table1.print_bill(10, 10)
+
+table2 = Tab()
+table2.add('wine')
+table2.add('beef')
+
+table2.print_bill(10, 10)
+
